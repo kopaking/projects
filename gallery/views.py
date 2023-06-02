403bc8c15590fb93c03613a0d0a75520cac6993d
kopaking <nicholas123458@gmail.com>
2023-06-02 17:51:56 -0300

    Mensagem do commit

---
+++ gallery/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'gallery/index.html')
+
+def imagem (request):
+    return render(request, 'gallery/imagem.html')
